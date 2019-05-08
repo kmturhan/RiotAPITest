6160402a1db598559062be7bed82da23b9fdca2d
kmturhan <kmturhan@gmail.com>
2019-05-08 06:32:50 +0300

    Itemlerin datalarının ayarlamaları için web api tarafı düzenlendi.

---
+++ RiotApiTest/Controllers/ItemsDataController.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RiotApiTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace RiotApiTest.Controllers
+{
+    public class ItemsDataController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage ItemList()
+        {
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.GetEncoding("UTF-8");
+            var json = wc.DownloadString("http://ddragon.leagueoflegends.com/cdn/9.9.1/data/tr_TR/item.json");
+ 
+            foreach (var item in JObject.Parse(json).SelectToken("data").Children())
+            {
+                Items data = JsonConvert.DeserializeObject<Items>(item.First.ToString());
+            }
+            return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(json) };
+        }
+    }
+}
---
+++ RiotApiTest/Models/Items.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RiotApiTest.Models
+{
+    public class Items
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PlainText { get; set; }
+        public string colloq { get; set; }
+        public List<string> into { get; set; }
+        public Image Image { get; set; }
+        public Gold Gold { get; set; }
+        public List<string> Tags { get; set; }
+        //public List<bool> Maps { get; set; }
+        public Dictionary<string,string> Stats { get; set; }
+    }
+    public class Image
+    {
+        public string Full { get; set; }
+        public string Sprite { get; set; }
+        public string Group { get; set; }
+    }
+    public class Gold
+    {
+        public int Base { get; set; }
+        public bool Purchasable { get; set; }
+        public int Total { get; set; }
+        public int Sell { get; set; }
+    }
+}
